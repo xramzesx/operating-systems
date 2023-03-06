1d5a56faf96b53fb59648804abae2040fe4513ed
xramzesx <xramzesbusiness@gmail.com>
2023-03-06 19:23:38 +0100

    [lab1] add repl interface

--- cw01/zad2/main_static.c
+++ cw01/zad2/main_static.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 #include "lib.h"
 
-#define BUFFER_SIZE 4096
+#define MAX_BUFFER_SIZE 4096
 
 enum Repl { 
     Init,    Count, 
@@ -12,12 +14,17 @@ enum Repl {
 typedef enum Repl Repl;
 
 Repl get_repl ( const char * command ) {
+    if ( NULL == command ) 
+        return Default;
 
     if ( 0 == strcmp(command, "init") )
         return Init;
     
     if ( 0 == strcmp(command, "count") )
         return Count;
+    
+    if ( 0 == strcmp(command, "show") )
+        return Show;
     
     if ( 0 == strcmp(command, "delete") )
         return Delete;
@@ -32,27 +39,33 @@ Repl get_repl ( const char * command ) {
 int main () {
 
     char buffer[MAX_LINE_LENGTH];
+    char filename[MAX_BUFFER_SIZE];
+
     Data * data = NULL;
+    size_t index = 0;
 
 
-    while ( fgets(buffer, BUFFER_SIZE, stdin )) {
+    while ( fgets(buffer, MAX_BUFFER_SIZE, stdin )) {
         char * argument;
         sscanf(buffer, "%s", argument);
 
+        char argument[BUFSIZ];
+        sscanf(buffer, "%s ", argument);
         to_lower(argument);
 
         switch ( get_repl(argument) ) {
             case Init:
+                size_t capacity = 0;
+
+                if ( sscanf( buffer + strlen(argument), " %ld", &capacity ) != 1) {
+                    error_log("invalid argument type");
+                    break;
+                }
+
                 if ( NULL != data ) {
                     destroy_data(data);
                     data = NULL;
-                }
-
-                size_t capacity = 0;
-
-                if ( sscanf( buffer + strlen(argument), "%ld", &capacity )) {
-                    error_log("ivalid type for argument");
-                    break;
+                    message_log("previous structure destroyed");
                 }
 
                 data = generate_data(capacity, 0);
@@ -61,23 +74,86 @@ int main () {
                 break;
             
             case Count:
-            
+                
+                if ( NULL == data ) {
+                    error_log("uninitialized structure, init first!");
+                    break;
+                }
+
+                
+                filename[0] = 0;
+
+                sscanf( buffer + strlen(argument), "%*[^\"]\"%[^\"]\"", filename );
+                
+                if ( strlen( filename ) == 0 && sscanf(buffer + strlen(argument), " %s", filename ) != 1 ) {
+                    error_log("invalid argument type");
+                    break;
+                }
+                
+                if (word_count(data, filename))
+                    message_log("words counted");
+                else 
+                    error_log("failed to count words");
                 break;
             
             case Show:
+
+                if ( NULL == data ) {
+                    error_log("initialize first!");
+                break;
+                }
+            
+                index = 0;
+            
+                if ( sscanf( buffer + strlen(argument), " %ld", &index ) != 1 ) {
+                    error_log("invalid argument type");
+                break;
+                };
+            
+                char * result = (char *) get_block(data, index); 
+                if ( result )
+                    printf("%s\n", result);
+                else
+                    printf("/empty/\n");
+                result = NULL;
             
                 break;
             
             case Delete:
+                if ( NULL == data ) {
+                    error_log("initialize first!");
+                    break;
+                }
+
+                index = 0;
+
+                if ( sscanf( buffer + strlen(argument), " %ld", &index ) != 1 ) {
+                    error_log("invalid argument type");
+                    break;
+                };
+
+                if (delete_block(data, index)) {
+                    message_log("block deleted");
+                } else {
+                    message_log("block is already empty");
+                }
             
                 break;
             
             case Destroy:
+            
+                if ( NULL == data ) {
+                    error_log("initialize first!");
+                    break;
+                }
+                
                     destroy_data(data);
                     data = NULL;
+                message_log("structure destroyed");
                 break;
             
             default:
+                error_log("unknown command");
                 break;
         }
 
