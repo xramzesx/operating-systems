99d4b4da516e0f6e21b3c0ea3f02f9cbe3266144
xramzesx <xramzesbusiness@gmail.com>
2023-03-27 13:00:53 +0200

    [lab4] initial zad3

---
+++ cw04/zad3/catcher.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+
+enum OperatingModes {
+    Range = 1,
+    Time,
+    Requests,
+    Interval,
+    Exit
+};
+
+typedef enum OperatingModes OperatingModes;
+
+void print_current_time(){
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    time ( &rawtime );
+    timeinfo = localtime ( &rawtime );
+    printf ( "Current local time and date: %s", asctime (timeinfo) );
+    fflush(stdout);
+}
+
+int requests_count = 0;
+int show_time = 0;
+
+void handler_sigusr1(
+    int sig,
+    siginfo_t *info,
+    void *ucontext
+) {
+    requests_count++;
+    show_time = 0;
+
+    printf("\n==:[Received]:=======\n");
+    printf(":[signal]:\t%d\n", info->si_signo);
+    printf(":[pid]:\t%d\n", info->si_pid);
+    printf(":[sigval]:\t%d\n", info->si_value.sival_int);
+    
+    fflush(stdout);
+
+    printf("Output:\n");
+
+    switch (info->si_value.sival_int) {
+    case Range:
+        for (int i = 1; i <= 100; i++ ) {
+            printf("%d, ", i);
+        }
+        printf("\n");
+        fflush(stdout);
+        break;
+    case Time:
+        print_current_time();
+        break;
+    case Requests:
+        printf("Requests count: %d\n", requests_count);
+        fflush(stdout);
+        break;
+    case Interval:
+        show_time = 1;
+        printf("Show current time\n");
+        break;
+    case Exit:
+        printf("process exit\n");
+        fflush(stdout);
+        kill(info->si_pid, SIGUSR1);
+        exit(0);
+        break;
+
+    default:
+        perror("invalid operating mode\n");
+        fflush(stdout);
+        break;
+    }
+    
+    // usleep(10000);
+
+    kill(info->si_pid, SIGUSR1);
+}
+
+
+int main () {
+
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = &handler_sigusr1;
+    sigaction(SIGUSR1, &sa, NULL);
+
+    printf("[catcher] pid: %d\n", getpid());
+
+    while (1) {
+        if ( show_time ) {
+            print_current_time();
+        }
+        sleep(1);
+    }
+
+    return 0;
+}
---
+++ cw04/zad3/sender.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+int is_number(const char * str) {
+
+    for (int i = 0; i < strlen(str); i++) {
+        if ( !isdigit (str[i]) )
+            return 0;
+    }
+
+    return 1;
+}
+
+void handler_sigusr1(
+    int sig
+) {
+
+}
+
+void send_signal( 
+    pid_t pid, 
+    int signum, 
+    int code
+) {
+    union sigval value;
+
+    value.sival_int = code;
+
+    sigset_t sigset;
+
+    sigfillset(&sigset);
+    sigdelset(&sigset, signum);
+    sigdelset(&sigset, SIGINT);
+
+    sigqueue(
+        pid, 
+        signum, 
+        value
+    );
+
+    sigsuspend(&sigset);
+    printf("received confirm: %d\n", code);
+    // if (sigsuspend(&sigset) == -1) {
+    //     perror("")
+    // }
+}
+
+int main (int argc, char ** argv) {
+
+    //// VALIDATION ////
+
+    if (argc < 3) {
+        perror("not enough arguments");
+        return 1;
+    }
+
+    if ( !is_number(argv[1]) ) {
+        perror("invalid pid");
+        return 1;
+    }
+
+    pid_t pid = atoi(argv[1]);
+
+    int n = argc - 2;
+    int * commands = calloc(sizeof(int), argc - 2);
+
+    for (int i = 2; i < argc; i++) {
+        if (!is_number(argv[i])) {    
+            free(commands);
+            perror("invalid operating mode");
+            return 1;
+        }
+
+        commands[i-2] = atoi(argv[i]);
+
+        if (commands[i-2] > 5 || commands[i-2] < 0) {
+            free(commands);
+            perror("operating mode not found");
+            return 1;
+        }
+
+        printf("%d\n", commands[i - 2]);
+
+    }
+
+
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_sigaction = &handler_sigusr1;
+    sigaction(SIGUSR1, &sa, NULL);
+
+    printf("[sender] pid: %d\n", getpid());
+
+
+    for (int i = 0; i < n; i++ ){ 
+        send_signal(pid, SIGUSR1, commands[i]);
+
+    }
+    
+
+    return 0;
+}
